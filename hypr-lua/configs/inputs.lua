8459e45b9434f2898b8d238f78a0cd769c15902c
netchunk <mhmiraj2005@gmail.com>
2026-09-03 17:03:18 +0600

    inputs: {lang: ara,bn,eng}, {qwerty:phonetic for arabic and bengali}, {swich language with alt+shift}

--- hypr-lua/configs/inputs.lua
+++ hypr-lua/configs/inputs.lua
@@ -4,10 +4,12 @@
 hl.config({
     input = {
         numlock_by_default = true,
-        kb_layout = "us",
-        kb_variant = "",
+        kb_layout = "us,ara,bd", -- def: us
+        kb_variant = ",buckwalter,probhat", -- def:
+        kb_options = "grp:alt_shift_toggle", -- def: ""
+            -- "caps:swapescape" for swapping tab and esc key with each other
+            -- "grp:alt_shift_toggle" for switching language with 'alt + shift'
         kb_model = "",
-        kb_options = "",
         kb_rules = "",
         follow_mouse = 1,
         sensitivity = 0,
